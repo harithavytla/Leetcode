3a8e57c536788b511e4f920266b49e5bd2d69329
harithavytla <haritha.vytla@gmail.com>
2020-05-04 16:45:50 +0530

    Inorder

--- Inorder_traversal.java
+++ Inorder_traversal.java
@@ -13,3 +13,22 @@ class Solution {
         return result;
     }
 }
+
+//Iteration
+
+class Solution {
+    List result=new ArrayList();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> s=new Stack();
+        while(root!=null||!s.isEmpty()){
+            while(root!=null){
+                s.push(root);
+                root=root.left;
+            }
+            root=s.pop();
+            result.add(root.val);
+            root=root.right;
+        }
+        return result;
+    }
+}
