de5a36031ebf84c7f0d2d5f046c0cec5ea6c5050
harithavytla <haritha.vytla@gmail.com>
2020-05-04 16:29:21 +0530

    Inorder

---
+++ Inorder_traversal.java
@@ -0,0 +1,15 @@
+class Solution {
+    List<Integer> result=new ArrayList();
+    public List<Integer> inorderTraversal(TreeNode root) {        
+        if(root!=null){
+            if(root.left!=null){
+                inorderTraversal(root.left);
+            }
+            result.add(root.val);
+            if(root.right!=null){
+                inorderTraversal(root.right);
+            }                        
+        }
+        return result;
+    }
+}
