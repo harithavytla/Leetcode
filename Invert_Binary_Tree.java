da648d0c16a9df0481b4568116a646d61cfa52fe
harithavytla <haritha.vytla@gmail.com>
2020-08-06 15:50:41 +0530

    485-Maximum Consecutive Ones in an Array

--- Invert_Binary_Tree.java
+++
@@ -1,8 +0,0 @@
-public TreeNode invertTree(TreeNode root) {
-        if(root==null){return null;}
-        TreeNode left=invertTree(root.left);
-        TreeNode right=invertTree(root.right);
-        root.right=left;
-        root.left=right;
-        return root;
-    }
